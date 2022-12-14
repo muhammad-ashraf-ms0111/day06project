9d4bd887c2922be0f2d975730897b344bcefcdfd
Muhammad-Ashraf-MS0111 <muhammad.ashraf.ms0111@gmail.com>
2022-12-14 14:30:47 +0800

    Update ListServer.java

--- src/ListServer.java
+++ src/ListServer.java
@@ -15,35 +15,36 @@ public class ListServer {
     // Random number generator
     Random rnd = new SecureRandom();
 
-    // Create a ServerSocket and bind to the port
-    ServerSocket server = new ServerSocket(port) ;
-    System.out.printf("Listening on port %d\n", port);
+    try (// Create a ServerSocket and bind to the port
+    ServerSocket server = new ServerSocket(port)) {
+        System.out.printf("Listening on port %d\n", port);
 
-    // Server loop
-    while (true) {
-        // Wait for a connection
-        System.out.println("Waiting for connections...");
-        Socket socket = server.accept();
+        // Server loop
+        while (true) {
+            // Wait for a connection
+            System.out.println("Waiting for connections...");
+            Socket socket = server.accept();
 
-        System.out.printf("New connection on port %d\n", socket.getPort());
+            System.out.printf("New connection on port %d\n", socket.getPort());
 
-        String payload = IOUtils.read(socket);
-        String[] values = payload.split(" ");
+            String payload = IOUtils.read(socket);
+            String[] values = payload.split(" ");
 
-        Integer count = Integer.parseInt(values[0]);
-        Integer range = Integer.parseInt(values[1]);
+            Integer count = Integer.parseInt(values[0]);
+            Integer range = Integer.parseInt(values[1]);
 
-        List<Integer> randNums = new LinkedList<>();
-        for (Integer i = 0; i < count; i++)
-            randNums.add(rnd.nextInt(range));
+            List<Integer> randNums = new LinkedList<>();
+            for (Integer i = 0; i < count; i++)
+                randNums.add(rnd.nextInt(range));
 
-        String response = randNums.stream()
-            .map(v -> v.toString())
-            .collect(Collectors.joining(":"));
+            String response = randNums.stream()
+                .map(v -> v.toString())
+                .collect(Collectors.joining(":"));
 
-        IOUtils.write(socket, response);
+            IOUtils.write(socket, response);
 
-        socket.close();
+            socket.close();
+        }
     }
 
 }
